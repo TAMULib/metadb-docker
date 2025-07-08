6ed8483e03d07e13ea67e9deb583f30ddea434a2
TrentTAMU <spears.trent@tamu.edu>
2025-07-08 11:45:45 -0500

    Add dependency updates for CVEs

--- modify-code.sh
+++ modify-code.sh
@@ -7,3 +7,8 @@ sed -i -e 's/if opt.ForceAll {/if opt.ForceAll || opt.Force {/g' ./cmd/metadb/ds
 # Allows derived table git repo to be customized via ENV variable.
 sed -i -e 's/url :\= \"https:\/\/github.com\/folio-org\/folio-analytics.git\"/url :\= os.Getenv("DERIVED_TABLES_GIT_REPO")/g' ./cmd/metadb/server/server.go
 sed -i -e 's/ref, err \= cat.GetConfig(\"external_sql_folio\")/ref \= os.Getenv("DERIVED_TABLES_GIT_TAG")/g' ./cmd/metadb/server/server.go
+
+# Update dependencies to address CVE-2025-22869, CVE-2025-22870, CVE-2025-22872, and GHSA-2x5j-vhc8-9cwm
+go get golang.org/x/crypto@v0.35.0
+go get golang.org/x/net@v0.38.0
+go get github.com/cloudflare/circl@v1.6.1

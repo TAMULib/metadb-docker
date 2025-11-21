1401fbf689bfc9d5e69ccd05169935fca59a7c81
Trent Spears <138056548+TrentTAMU@users.noreply.github.com>
2025-11-21 12:12:48 -0600

    Address CVE-2025-47914 and CVE-2025-58181

--- modify-code.sh
+++ modify-code.sh
@@ -4,7 +4,6 @@
 sed -i -e 's/if percent \> 20.0 {/if percent \> 20.0 \&\& !opt.Force {/g' ./cmd/metadb/dsync/endsync.go
 sed -i -e 's/if syncMode != NoSync {/if syncMode != NoSync \&\& !opt.Force {/g' ./cmd/metadb/dsync/sync.go
 
-
 # Allows derived table git repo to be customized via ENV variable.
 sed -i -e 's/url :\= \"https:\/\/github.com\/folio-org\/folio-analytics.git\"/url :\= os.Getenv("DERIVED_TABLES_GIT_REPO")/g' ./cmd/metadb/server/server.go
 sed -i -e 's/ref :\= util.GetFolioVersion()/ref :\= os.Getenv("DERIVED_TABLES_GIT_REFS")/g' ./cmd/metadb/server/server.go
@@ -12,9 +11,9 @@ sed -i -e 's/ref :\= util.GetFolioVersion()/ref :\= os.Getenv("DERIVED_TABLES_GI
 # Remove manual prompt from endsync process
 sed -i -e 's/if !opt.Force {/if true == false {/g' ./cmd/metadb/dsync/endsync.go
 
-# HOTFIX: Changes library versions to address CVE-2025-58187, CVE-2025-58188, CVE-2025-21613, CVE-2024-45337, GHSA-9763-4f94-gfch, CVE-2025-21614, CVE-2025-22869, CVE-2023-45288, CVE-2025-22870, CVE-2025-22872
+# HOTFIX: Changes library versions to address CVE-2025-58187, CVE-2025-58188, CVE-2025-21613, CVE-2024-45337, GHSA-9763-4f94-gfch, CVE-2025-21614, CVE-2025-22869, CVE-2023-45288, CVE-2025-22870, CVE-2025-22872, CVE-2025-47914, CVE-2025-58181
 go env -w GOTOOLCHAIN=go1.24.9+auto
 go get github.com/go-git/go-git/v5@v5.13.0
-go get golang.org/x/crypto@v0.35.0
+go get golang.org/x/crypto@v0.45.0
 go get github.com/cloudflare/circl@v1.6.1
 go get golang.org/x/net@v0.38.0

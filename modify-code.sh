132e9978bea80e90029b00ac21e9ee3ee8708779
Trent Spears <spears.trent@tamu.edu>
2026-02-19 14:58:40 -0600

    Address CVE-2026-25934

--- modify-code.sh
+++ modify-code.sh
@@ -14,7 +14,7 @@ sed -i -e 's/if !opt.Force {/if true == false {/g' ./cmd/metadb/dsync/endsync.go
 # HOTFIX: Changes library versions to address CVE-2025-58187, CVE-2025-58188, CVE-2025-21613, CVE-2024-45337, GHSA-9763-4f94-gfch, CVE-2025-21614, CVE-2025-22869, CVE-2023-45288, CVE-2025-22870, CVE-2025-22872, CVE-2025-47914, CVE-2025-58181
 
 go env -w GOTOOLCHAIN=go1.24.13+auto
-go get github.com/go-git/go-git/v5@v5.13.0
+go get github.com/go-git/go-git/v5@v5.16.5
 go get golang.org/x/crypto@v0.45.0
 go get github.com/cloudflare/circl@v1.6.1
 go get golang.org/x/net@v0.38.0
